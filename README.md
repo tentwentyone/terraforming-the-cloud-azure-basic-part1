1c6031541ce9eddf896ba874028563240bfa923a
Filipe Rocha <filipe.m.rocha@nos.pt>
2024-05-29 14:44:38 +0100

    Cleaning up repository for public access

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Owner-avatar-terraforming-the-cloud-azure-part1
+
 Terraform workshop series: Terraform'ing the cloud - AZURE VERSION - Part 1
--- variables.tf
+++ variables.tf
@@ -1,42 +1,19 @@
-# variable "project_id" {
-#   type        = string
-#   description = "The google project identifier."
-# }
-
-# variable "region" {
-#   type        = string
-#   description = "The default region to use."
-#   default     = "europe-west1"
-# }
-
-# variable "prefix" {
-#   type        = string
-#   description = "A simple prefix"
-#   default     = "gcp"
-# }
-
-## --------------------------- AZURE VERSION --------------------------- 
-
 variable "tenant_id" {
   type        = string
   description = "This is the default tenant id where the resources are managed."
-  default     = "930a470b-f629-4f93-a65b-bad14153f14f"
 }
 
 variable "subscription_id" {
   type        = string
   description = "The Azure subscriptgaaion identifier."
-  default     = "a5a70928-a6c8-4495-b752-a7ab1d7768b7" # ccoe-lab
 }
 
 variable "region" {
   type        = string
   description = "The default region to use."
-  default     = "westeurope"
 }
 
 variable "prefix" {
   type        = string
   description = "A simple prefix"
-  default     = "azure"
 }
