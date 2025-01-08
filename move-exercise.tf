85df922922bc386d4882e2ed8e22c35990fa7691
Miguel Nunes <miguel.a.nunes@parceiros.nos.pt>
2025-01-08 09:12:48 +0000

    Configuring codetour

--- move-exercise.tf
+++ move-exercise.tf
@@ -1,13 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ## 3.3 Descomentar apenas quando for pedido.
 
+## 3.3.1
 # resource "random_pet" "new_id" {
 #   length    = 2
 #   separator = "-"
 #   prefix    = var.prefix
 # }
 
-# ## Descomentar após a criação do random_pet acima
-# # moved {
-# #   from = random_pet.new_id
-# #   to = random_pet.moved
-# # }
+## 3.3.2
+## Descomentar após a criação do random_pet acima
+# moved {
+#   from = random_pet.new_id
+#   to = random_pet.moved
+# }
